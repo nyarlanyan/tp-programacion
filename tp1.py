9d932c88e321a17e0a63638e9015250932ed6173
daiana <luna_padoru@hotmail.com>
2023-05-22 11:11:14 -0300

    "primer cambio"

---
+++ tp1.py
@@ -0,0 +1,43 @@
+a = 4
+b = 2
+c = a + b
+print (c)
+
+
+
+
+# realize un programa que lea 4 numeros y diga cuantos pares son y cuantos impares y devuelva la sumatoria de los pares
+
+contador = 0
+pares = 0
+suma_pares = 0
+
+while contador < 4:
+    numero = int(input("Ingrese un número: "))
+    if numero % 2 == 0:
+        pares += 1
+        suma_pares += numero
+    contador += 1
+
+impares = 4 - pares
+
+print("Cantidad de números pares:", pares)
+print("Cantidad de números impares:", impares)
+print("Suma de los números pares:", suma_pares)
+
+
+# leer 10 numeros y obtener la cantidad de mayores y la cantidad de menores a 100 , cual es el numero maximo y cual es el numero minimo
+
+
+
+
+# ingresar las edas y el sexo de 25 personas y determinar cuantas son mujeres, cuantos varones, cuantos mayores de edad y cuantos menores de edad
+ 
+
+# leer 10 numeros y mostrar solamente los numeros positivos y su sumatoria 
+
+
+# leer 15 numeros negativos y convertirlos a positivos e imprimir dichos numeros
+
+
+
