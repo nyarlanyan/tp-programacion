c0374e59395cd6dd0d1ec910c542f644bc96ec78
daiana <luna_padoru@hotmail.com>
2023-07-03 17:35:19 -0300

    ejercicios

--- tp1.py
+++ tp1.py
@@ -1,43 +1,22 @@
-a = 4
-b = 2
-c = a + b
-print (c)
 
 
+# Realice un programa que contenga una función que se llame “conversion”, que la misma contenga tres parámetros
+# Se pide convertir los segundos ingresados en horas, minutos y segundos
 
+def conversion(segundos):
+   horas = segundos // 3600 
+   segundos %= 3600
+   minutos = segundos// 60
+   segundos %= 60
+   return horas, minutos, segundos
 
-# realize un programa que lea 4 numeros y diga cuantos pares son y cuantos impares y devuelva la sumatoria de los pares
+segundos_totales = int(input ("ingrese la cantidad de segundos")) 
 
-contador = 0
-pares = 0
-suma_pares = 0
+horas, minutos, segundos = conversion( segundos_totales) 
 
-while contador < 4:
-    numero = int(input("Ingrese un número: "))
-    if numero % 2 == 0:
-        pares += 1
-        suma_pares += numero
-    contador += 1
-
-impares = 4 - pares
-
-print("Cantidad de números pares:", pares)
-print("Cantidad de números impares:", impares)
-print("Suma de los números pares:", suma_pares)
-
-
-# leer 10 numeros y obtener la cantidad de mayores y la cantidad de menores a 100 , cual es el numero maximo y cual es el numero minimo
-
-
-
-
-# ingresar las edas y el sexo de 25 personas y determinar cuantas son mujeres, cuantos varones, cuantos mayores de edad y cuantos menores de edad
+print(f"{segundos_totales} segundos equivale a:{horas} horas, {minutos} minutos, {segundos} segundos," )
  
 
-# leer 10 numeros y mostrar solamente los numeros positivos y su sumatoria 
 
 
-# leer 15 numeros negativos y convertirlos a positivos e imprimir dichos numeros
-
-
-
+ 
